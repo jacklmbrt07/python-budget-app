71f4e31d9cf243db41a95f4ec9857e2b020036dd
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-02 12:44:09 -0800

    may be getting somewhere

--- budget.py
+++ budget.py
@@ -1,34 +1,27 @@
 # categories = [food, business, entertainment, clothing]
 
 class Category:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.ledger = []
     
     def deposit(self, amount, description=None):
         if description == None:
-            return ""
+            self.ledger.append({"amount": amount})
         else:
-            self.ledger.append({"amount": amount, "description": description}) 
+            self.ledger.append({"amount": amount, "description": description})
+        print(self.ledger)
     
     def withdraw(self, amount, balance):
-        if amount < balance:
-            self.ledger.append({"amount": -amount})
-            return True
-        else:
-            return False
-        
+        pass
+    
     def get_balance(self):
-        # returns current balance of budget based on deposits and withdrawls
         pass
     
     def transfer(self, amount, category):
-        # should return description "Transfer to [Destination Budget Category]"
-        # then add a deposit to the other budget category with the amount, description "Transfer from [Source Budget Category]".
-        # f there are not enough funds, nothing should be added to either ledgers. This method should return True if the transfer took place, and False otherwise.
         pass
     
     def check_funds(self, amount):
-        # returns False if the amount is greater than the balance of the budget category and returns True otherwise. This method should be used by both the withdraw method and transfer method.
         pass
     
 
